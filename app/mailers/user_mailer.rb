8a11e4fbe9295f18d75d517a8df9e9608bcb0f35
shunkurosaki <shunkurosakisk@gmail.com>
2015-05-07 19:08:29 +0900

    devise confirm function

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.registration_confirmation.subject
+  #
+  def registration_confirmation
+    @greeting = "Hi"
+
+    mail to: "shunkurosakisk@gmail.com"
+  end
+end
