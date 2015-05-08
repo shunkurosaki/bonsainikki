0b6e60774beb5f6b5b1da093264deb2690405544
shunkurosaki <shunkurosakisk@gmail.com>
2015-05-09 01:34:18 +0900

    fix home#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: '投稿が作成されました。.' }
+        format.html { redirect_to @post, notice: '投稿が作成されました。' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
