3c198faccd4ef9d016526cb1789e4e8e111450ac
shunkurosaki <shunkurosakisk@gmail.com>
2015-05-08 20:47:14 +0900

    fix home#index design

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
   def index
-    @all_posts = Post.all
+    @all_posts = Post.all.order("id DESC")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
   resources :homes, only: [:index]
   resources :posts
+  resources :users, only: [ :show]
   root 'homes#index'
 end
