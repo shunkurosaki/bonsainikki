b66000abc979a4f425af2fc76c15130ab215efee
shunkurosaki <shunkurosakisk@gmail.com>
2015-05-10 16:24:02 +0900

    complete sitemap settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'users/show'
+  get 'sitemap', to: redirect('http://s3-ap-northeast-1.amazonaws.com/bonsainikki.pro/sitemaps/sitemap.xml.gz')
 
   devise_for :users
   resources :homes, only: [:index]
