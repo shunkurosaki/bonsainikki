76d96d337819221e0812516e8304a9693610744f
shunkurosaki <shunkurosakisk@gmail.com>
2015-05-10 15:43:11 +0900

    seo: add sitemap

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  # storage :fog
+  # Amazon S3
+    storage :fog if Rails.env.production?
+  # Local strage
+    storage :file if Rails.env.test? or Rails.env.development?
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
