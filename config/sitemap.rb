c1cdba54cc3493fdeb3050bf5fbdc19ee3a638f4
shunkurosaki <shunkurosakisk@gmail.com>
2015-05-10 16:00:09 +0900

    fix

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,6 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://bonsainikki.com"
-SitemapGenerator::Sitemap.sitemaps_host = "https://bonsainikki.sitemap.s3.amazonaws.com/"
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/bonsainikki.sitemap"
 SitemapGenerator::Sitemap.public_path = 'public/'
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
